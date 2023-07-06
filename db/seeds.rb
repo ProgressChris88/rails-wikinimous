b70190a1eb7009a5d9c16533979a3e23afe7823d
Christopher Bowman <chrisbowmanfl@gmail.com>
2023-07-06 20:22:42 +0900

    Draft finished for wiki, will return to later

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Making articles ...'
+10.times do
+  articles = Article.create([
+  title: Faker::Movie.title,
+  content: Faker::Movie.quote
+  ])
+  puts "Finished #{Article.count} articles!"
+end
